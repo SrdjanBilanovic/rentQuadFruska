3092b27be0e3eecf130e19fdef130dfe08ac7e2c
SrdjanBilanovic <ruma4002@gmail.com>
2023-03-16 12:34:48 +0100

    forma prva deo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { FooterComponent } from './footer/footer.component';
+import { ReservationComponent } from './reservation/reservation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
     NavBarComponent,
-    FooterComponent
+    FooterComponent,
+    ReservationComponent
   ],
   imports: [
     BrowserModule,
