056a36e4ccd182722f22afa1a63935fd0055aab7
SrdjanBilanovic <ruma4002@gmail.com>
2023-03-19 20:03:18 +0100

    obrisano rutiranje

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ReservationComponent } from './reservation/reservation.component';
-import { FooterComponent } from './footer/footer.component';
 
-const routes: Routes = [
-  {path:'rezervacije',component:ReservationComponent},
-  {path:'kontakt', component:FooterComponent},
-  {path:"", redirectTo:'/rezervacije',pathMatch:'full'}
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
